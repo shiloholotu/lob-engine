f06522a54f76fdf63118cd7d751948e586ee29f0
Shiloh Olotu <shiloholotu@gmail.com>
2026-08-30 08:16:15 -0500

    test: reuse a released order-pool slot

--- tests/test_orderpool.cpp
+++ tests/test_orderpool.cpp
@@ -11,3 +11,14 @@ TEST(OrderPool, AllocateTwiceGivesDifferentSlots) {
     EXPECT_NE(a, b);
 }
 
+TEST(OrderPool, DeallocateThenAllocateReusesASlot) {
+    OrderPool pool;
+    Order* a = pool.allocate();
+    ASSERT_NE(a, nullptr);
+
+    pool.deallocate(a);
+
+    Order* b = pool.allocate();
+    ASSERT_NE(b, nullptr);
+    EXPECT_EQ(b, a);  // you pop_back, so the last freed index is reused first
+}
